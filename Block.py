fd903368a589c5e4ad74c9a645858342ee29ffb7
despoinavdl <despoinavdl@gmail.com>
2024-03-14 18:29:56 +0200

    Update Block.py

--- Block.py
+++ Block.py
@@ -13,12 +13,25 @@ class Block:
         self.current_hash = self.hash()
     
     def hash(self):
-        block_dict = {
+        block_dictionary = self.block_dict()
+        block_json = json.dumps(block_dictionary)
+        return hashlib.sha256(str(block_json).encode()).hexdigest()
+    
+    def add_transaction(self, transaction):
+        if (len(self.transactions) < self.capacity):
+            self.transactions.append(transaction)
+        else:
+            print("Block full")
+            #create new block ?
+
+    def block_dict(self):
+        block_dictionary = {
             'index': self.index,
             'timestamp': self.timestamp,
             'transactions': [transaction.to_dict() for transaction in self.transactions],
             'validator': self.validator,
             'previous_hash': self.previous_hash
         }
-        block_json = json.dumps(block_dict)
-        return hashlib.sha256(str(block_json).encode()).hexdigest()
+        return block_dictionary
+
+    
