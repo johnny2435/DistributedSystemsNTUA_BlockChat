03d6991742fdba689f3c2917744aecf9aeeadc87
despoinavdl <61871415+despoinavdl@users.noreply.github.com>
2024-04-05 18:48:36 +0300

    Update Wallet.py

--- Wallet.py
+++ Wallet.py
@@ -6,17 +6,16 @@ class Wallet:
         key = RSA.generate(keylength)
         self.private_key = key.export_key() #use .decode() to decode it
         self.public_key = key.publickey().export_key() #use .decode() to decode it
-        self.utxos = {}
+        self.utxos = []
+        self.utxos_soft = []
 
     def get_public_key(self):
         # Return the public key
         return self.public_key
 
     def get_balance(self):
-        # Calculate the wallet balance
-        return sum(self.utxos.values())
-    
-    def add_utxo(self, transaction_id, amount):
-        # Add a UTXO to the wallet
-        self.utxos[transaction_id] = amount
-
+      balance = 0
+      for tx in self.utxos:
+        if tx.address == self.public_key:
+          balance += tx.amount
+      return balance
