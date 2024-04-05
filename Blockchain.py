a4f18d0624e89ac26ee66547580916cbe75b204c
despoinavdl <61871415+despoinavdl@users.noreply.github.com>
2024-04-05 18:45:58 +0300

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -1,13 +1,16 @@
 class Blockchain:
-    def __init__(self, capacity):
-        self.blocks = []
+  def __init__(self, capacity):
+      self.blocks = []
 
-    def add_block(self, block):
-        self.blocks.append(block)
-        return
-    
-    def to_dict(self):
-        data = {
-            'blocks' : [x.to_dict() for x in self.blocks]
-        }
-        return data
+  def add_block(self, block):
+      self.blocks.append(block)
+      return
+
+  def get_last_block(self):
+      return self.blocks[-1]
+
+  def to_dict(self):
+      data = {
+          'blocks' : [x.to_dict() for x in self.blocks]
+      }
+      return data
